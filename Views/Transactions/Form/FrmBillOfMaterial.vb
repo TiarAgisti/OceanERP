e41e5bee9d3001e8f4995a83e999190e88f8820a
TGN01-PBTGROUP\tiara021600167 <tiar.agisti@gmail.com>
2017-08-28 15:11:28 +0700

    update frmlistbom

--- Views/Transactions/Form/FrmBillOfMaterial.vb
+++ Views/Transactions/Form/FrmBillOfMaterial.vb
@@ -2,4 +2,56 @@
 #Region "Declaration"
     Public conditionBOM As String
 #End Region
+
+#Region "ComboBox"
+
+#End Region
+
+#Region "Grid Detail"
+
+#End Region
+
+#Region "Clear Data"
+
+#End Region
+
+#Region "Validation Number"
+
+#End Region
+
+#Region "Add Grid Detail"
+
+#End Region
+
+#Region "Delete Grid"
+
+#End Region
+
+#Region "Check Empty"
+
+#End Region
+
+#Region "Check Available In List"
+
+#End Region
+
+#Region "Set Data"
+
+#End Region
+
+#Region "Function"
+
+#End Region
+
+#Region "Button"
+
+#End Region
+
+#Region "KeyPress"
+
+#End Region
+
+#Region "Row State Change"
+
+#End Region
 End Class
